cd55bba32644319b2373e29cc96f6d3b2bd4704f
Susana-Gutierrez <susanagabriela_ap@hotmail.com>
2021-07-12 19:19:13 -0700

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,27 @@
+var fullName;
+var isCool;
+var totalPets;
+
+fullName = 'Susana Gutierrez';
+isCool = true;
+totalPets = 1;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets: ', typeof totalPets);
+
+var unknown;
+
+unknown = null;
+
+console.log('value of unknown: ', unknown);
+console.log('typeof unknown: ', typeof unknown);
+
+var never;
+
+console.log('value of never: ', never);
+console.log('typeof never: ', typeof never);
